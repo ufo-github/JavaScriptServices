7ded0a0b60ce3a5356417a5d6102a376fe5f6802
SteveSandersonMS <stevesa@microsoft.com>
2016-12-01 15:13:38 +0000

    Example of Angular 2 lazy loading to help with #465

--- templates/Angular2Spa/webpack.config.js
+++ templates/Angular2Spa/webpack.config.js
@@ -13,7 +13,7 @@ var sharedConfig = {
     },
     module: {
         loaders: [
-            { test: /\.ts$/, include: /ClientApp/, loaders: ['ts-loader?silent=true', 'angular2-template-loader'] },
+            { test: /\.ts$/, include: /ClientApp/, loaders: ['ts-loader?silent=true', 'angular2-template-loader', 'angular2-router-loader'] },
             { test: /\.html$/, loader: 'html-loader?minimize=false' },
             { test: /\.css$/, loader: 'to-string-loader!css-loader' },
             { test: /\.(png|jpg|jpeg|gif|svg)$/, loader: 'url-loader', query: { limit: 25000 } }
